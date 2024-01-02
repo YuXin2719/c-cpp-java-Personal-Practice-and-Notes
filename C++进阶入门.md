27daa669fccbdb4c5213a951c1a536dc0d10f8fc
宇昕 <824279223@qq.com>
2024-01-02 22:25:04 +0800

    承接笔记

--- C++进阶入门.md
+++ C++进阶入门.md
@@ -4427,7 +4427,7 @@ int main()
 
 
 - 访问子类同名成员 直接访问即可
-- 访问子类同名成员 需要加作用域
+- 访问父类同名成员 需要加作用域
 
 
 
@@ -4829,6 +4829,181 @@ int main()
 - 代码组织结构清晰
 - 可读性强
 - 利于前期和后期的拓展以及维护
+
+
+
+**示例：**
+
+```c++
+#include <iostream>
+using namespace std;
+
+//分别利用普通写法和多态技术实现计算器
+
+//普通写法
+class Calculator
+{
+public:
+	int m_Num1; //操作数1
+	int m_Num2; //操作数2
+
+	int getResult(string oper)
+	{
+		if (oper == "+")
+		{
+			return m_Num1 + m_Num2;
+		}
+		else if (oper == "-")
+		{
+			return m_Num1 - m_Num2;
+		}
+		else if (oper == "*")
+		{
+			return m_Num1 * m_Num2;
+		}
+	}
+	//如果想拓展新的功能，需要修改源码
+	//在真实的开发中，提倡 开闭原则
+	//开闭原则：对拓展进行开放，对修改进行关闭
+};
+
+
+//利用多态实现计算器
+
+//多态好处：
+//1.组织结构清晰
+//2.可读性强
+//3.对于前期和后期的拓展和维护性高
+
+
+//实现计算器的抽象类
+class abstractCalculator //abstract(抽象)Calculator(计算器)
+{
+public:
+	int m_Num1;
+	int m_Num2;
+
+	virtual int getResult()
+	{
+		return 0;
+	}
+};
+
+//加法计算器类
+class addCalculator :public abstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 + m_Num2;
+	}
+};
+
+//减法计算器类
+class subCalculator :public abstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 - m_Num2;
+	}
+};
+
+//乘法计算器类
+class mulCalculator :public abstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 * m_Num2;
+	}
+};
+
+void test01()
+{
+	//创建计算器对象
+	Calculator c;
+	c.m_Num1 = 10;
+	c.m_Num2 = 10;
+
+	cout << c.m_Num1 << "+" << c.m_Num2 << "=" << c.getResult("+") << endl;
+	cout << c.m_Num1 << "-" << c.m_Num2 << "=" << c.getResult("-") << endl;
+	cout << c.m_Num1 << "*" << c.m_Num2 << "=" << c.getResult("*") << endl;
+}
+
+void test02()
+{
+	//多态使用条件
+	//父类指针或者引用指向子类对象
+
+
+	//加法运算
+	abstractCalculator* abc = new addCalculator;
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+
+	cout << abc->m_Num1 << "+" << abc->m_Num2 << "=" << abc->getResult() << endl;
+	//用完后记得销毁
+	delete abc;
+
+
+	//减法运算
+	abc = new subCalculator; //刚才销毁的是堆区的数据，指针的类型还没有变
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+
+	cout << abc->m_Num1 << "-" << abc->m_Num2 << "=" << abc->getResult() << endl;
+	delete abc;
+
+
+	//乘法运算
+	abc = new mulCalculator;
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+
+	cout << abc->m_Num1 << "*" << abc->m_Num2 << "=" << abc->getResult() << endl;
+}
+
+int main()
+{
+
+	//test01();
+
+	test02();
+
+	system("pause");
+	return 0;
+}
+```
+
+> 总结：C++开发提倡利用多态设计程序架构，因为多态优点很多
+
+
+
+#### 4.7.3 纯虚函数和抽象类
+
+
+
+在多态中，通常父类中虚函数的实现是毫无意义的，主要都是调用子类重写的内容
+
+
+
+因此可以将函数改为**纯虚函数**
+
+
+
+纯虚函数语法：**`virtual 返回值类型 函数名 （参数列表） = 0`**
+
+
+
+当类中有了纯虚函数，这个类也称为==抽象类==
+
+
+
+**抽象类特点：**
+
+- 无法实例化对象
+- 子类必须重写抽象类中的纯虚函数，否则也属于抽象类
 
 
 
--- cppCode/对象的初始化和清理/多态案例一 - 计算机类.cpp
+++ cppCode/对象的初始化和清理/多态案例一 - 计算机类.cpp
@@ -1,37 +1,139 @@
-#include <iostream>
-using namespace std;
-
-//分别利用普通写法和多态技术实现计算器
-
-//普通写法
-class Calculator
-{
-public:
-	int m_Num1; //操作数1
-	int m_Num2; //操作数2
-
-	int getResult(string oper)
-	{
-		if (oper == "+")
-		{
-			return m_Num1 + m_Num2;
-		}
-		else if (oper == "-")
-		{
-			return m_Num1 - m_Num2;
-		}
-		else if (oper == "*")
-		{
-			return m_Num1 * m_Num2;
-		}
-	}
-};
-
-int main()
-{
-
-	
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//
+////分别利用普通写法和多态技术实现计算器
+//
+////普通写法
+//class Calculator
+//{
+//public:
+//	int m_Num1; //操作数1
+//	int m_Num2; //操作数2
+//
+//	int getResult(string oper)
+//	{
+//		if (oper == "+")
+//		{
+//			return m_Num1 + m_Num2;
+//		}
+//		else if (oper == "-")
+//		{
+//			return m_Num1 - m_Num2;
+//		}
+//		else if (oper == "*")
+//		{
+//			return m_Num1 * m_Num2;
+//		}
+//	}
+//	//如果想拓展新的功能，需要修改源码
+//	//在真实的开发中，提倡 开闭原则
+//	//开闭原则：对拓展进行开放，对修改进行关闭
+//};
+//
+//
+////利用多态实现计算器
+//
+////多态好处：
+////1.组织结构清晰
+////2.可读性强
+////3.对于前期和后期的拓展和维护性高
+//
+//
+////实现计算器的抽象类
+//class abstractCalculator //abstract(抽象)Calculator(计算器)
+//{
+//public:
+//	int m_Num1;
+//	int m_Num2;
+//
+//	virtual int getResult()
+//	{
+//		return 0;
+//	}
+//};
+//
+////加法计算器类
+//class addCalculator :public abstractCalculator
+//{
+//public:
+//	int getResult()
+//	{
+//		return m_Num1 + m_Num2;
+//	}
+//};
+//
+////减法计算器类
+//class subCalculator :public abstractCalculator
+//{
+//public:
+//	int getResult()
+//	{
+//		return m_Num1 - m_Num2;
+//	}
+//};
+//
+////乘法计算器类
+//class mulCalculator :public abstractCalculator
+//{
+//public:
+//	int getResult()
+//	{
+//		return m_Num1 * m_Num2;
+//	}
+//};
+//
+//void test01()
+//{
+//	//创建计算器对象
+//	Calculator c;
+//	c.m_Num1 = 10;
+//	c.m_Num2 = 10;
+//
+//	cout << c.m_Num1 << "+" << c.m_Num2 << "=" << c.getResult("+") << endl;
+//	cout << c.m_Num1 << "-" << c.m_Num2 << "=" << c.getResult("-") << endl;
+//	cout << c.m_Num1 << "*" << c.m_Num2 << "=" << c.getResult("*") << endl;
+//}
+//
+//void test02()
+//{
+//	//多态使用条件
+//	//父类指针或者引用指向子类对象
+//
+//
+//	//加法运算
+//	abstractCalculator* abc = new addCalculator;
+//	abc->m_Num1 = 100;
+//	abc->m_Num2 = 100;
+//
+//	cout << abc->m_Num1 << "+" << abc->m_Num2 << "=" << abc->getResult() << endl;
+//	//用完后记得销毁
+//	delete abc;
+//
+//
+//	//减法运算
+//	abc = new subCalculator; //刚才销毁的是堆区的数据，指针的类型还没有变
+//	abc->m_Num1 = 100;
+//	abc->m_Num2 = 100;
+//
+//	cout << abc->m_Num1 << "-" << abc->m_Num2 << "=" << abc->getResult() << endl;
+//	delete abc;
+//
+//
+//	//乘法运算
+//	abc = new mulCalculator;
+//	abc->m_Num1 = 100;
+//	abc->m_Num2 = 100;
+//
+//	cout << abc->m_Num1 << "*" << abc->m_Num2 << "=" << abc->getResult() << endl;
+//}
+//
+//int main()
+//{
+//
+//	//test01();
+//
+//	test02();
+//
+//	system("pause");
+//	return 0;
+//}
