57497481d00917d1016aae252c0b1f6a35bf01f0
宇昕 <824279223@qq.com>
2025-01-17 04:44:33 +0800

    日常更新

--- cppCode/C++基础练习/测试代码.cpp
+++ cppCode/C++基础练习/测试代码.cpp
@@ -5,10 +5,26 @@ using namespace std;
 int main()
 {
 
-	int a = 1;
-	int b = 3;
-	double c = a / b;
-	cout << c << endl;
+	int arr[] = { 1,2,3,4,5 };
+	int start = 0;
+	int end = (sizeof(arr) / sizeof(arr[0]) - 1);
+	while (1) {
+		if (end > start) {
+			int tmp;
+			tmp = arr[end];
+			arr[end] = arr[start];
+			arr[start] = tmp;
+			end -= 1;
+			start += 1;
+		}
+		else {
+			break;
+		}
+	}
+
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+		cout << arr[i];
+	}
 
 	system("pause");
 	return 0;
